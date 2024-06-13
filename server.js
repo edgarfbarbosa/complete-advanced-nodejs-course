83d321554882f50bd7e1e0e97f2b854abc45924d
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-06-13 13:17:27 -0300

    feat(server): adiciona configuração de conexão com MongoDB

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Requere o mongoose para interagir com o banco de dados MongoDB
+const mongoose = require('mongoose');
 // Requere o módulo dotenv para carregar variáveis de ambiente a partir de um arquivo .env
 const dotenv = require('dotenv');
 // Carrega variáveis de ambiente do arquivo config.env
@@ -5,6 +7,21 @@ dotenv.config({ path: './config.env' });
 
 // Importa aplicação Express configurada do arquivo app.js
 const app = require('./app');
+
+// Constrói a string de conexão com o banco de dados MongoDB substituindo a senha na URI
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// Conecta ao banco de dados MongoDB
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const hostname = '127.0.0.1';
