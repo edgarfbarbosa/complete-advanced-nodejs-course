da75afc6d651af12924d82e8e2f36a993b4e7614
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-06-14 12:57:20 -0300

    feat: adiciona definição de esquema e modelo Tour no código

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
+// Definição do esquema para os documentos 'Tour' no MongoDB
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Criação do modelo 'Tour' baseado no esquema definido
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 const hostname = '127.0.0.1';
 /**
