161afc9152f332883f6c9a513cd8b83074f04236
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-06-17 18:20:20 -0300

    feat: simplifica definição de rotas no arquivo de roteamento

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,13 +5,13 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 // Middleware para verificar ID nas rotas de passeios.
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 /** Define a rota '/api/v1/tours' com métodos GET e POST. */
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour); // O middleware `checkBody` é executado primeiro para garantir que a requisição tenha os dados necessários.
+  .post(tourController.createTour);
 /** Define a rota '/api/v1/tours/:id' com métodos GET, PATCH e DELETE. */
 router
   .route('/:id')
