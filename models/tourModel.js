f49c4fef4e234759a5ab0ca2ee359c953e9a4184
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-07-20 14:38:08 -0300

    feat: adiciona validação personalizada para garantir que o valor do desconto seja menor que o preço

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 const slugify = require('slugify');
 
+const validator = require('validator');
+
 // Definição do esquema para os documentos 'Tour' no MongoDB
 const tourSchema = new mongoose.Schema(
   {
@@ -13,6 +15,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
       minlength: [10, 'A tour name must have more or equal then 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     duration: {
       type: Number,
@@ -44,7 +47,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
