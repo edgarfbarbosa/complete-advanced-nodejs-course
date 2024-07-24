02e3274a912c516a06feb7de84400f2ecda5c235
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-07-23 21:28:15 -0300

    feat: adiciona middleware global de tratamento de erros

--- app.js
+++ app.js
@@ -51,6 +51,26 @@ app.all('*', (req, res, next) => {
     status: 'fail',
     message: `Can't find ${req.originalUrl} on this server!`,
   });
+
+  /*
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+  */
+});
+
+app.use((err, req, res, next) => {
+  // Define um statusCode para o erro, usando 500 como padrão se não estiver definido
+  err.statusCode = err.statusCode || 500;
+  // Define um status para o erro, usando 'error' como padrão se não estiver definido
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 // Exporta a aplicação Express
