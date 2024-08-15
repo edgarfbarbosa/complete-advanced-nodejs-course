511890ebed7e195de2b0d1343effa6aae019086f
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-08-15 11:36:25 -0300

    feat: implementa tratamento para exceções não detectadas e rejeições não tratadas

--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 const mongoose = require('mongoose');
 // Requere o módulo dotenv para carregar variáveis de ambiente a partir de um arquivo .env
 const dotenv = require('dotenv');
+
+/** Captura exceções não detectadas (erros fora de blocos try-catch): */
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Exceção não detectada!');
+
+  process.exit(1); // Encerra o processo com um código de erro (1 indica finalização anormal)
+});
+
 // Carrega variáveis de ambiente do arquivo config.env
 dotenv.config({ path: './config.env' });
 
@@ -30,6 +39,16 @@ const hostname = '127.0.0.1';
  * @param {number} port - Porta em que o servidor vai escutar.
  * @param {string} hostname - Hostname onde o servidor vai rodar.
  */
-app.listen(port, hostname, () => {
+const server = app.listen(port, hostname, () => {
   console.log(`App running on http://${hostname}:${port}`);
+});
+
+/** Captura rejeições não tratadas (erros em Promises que não foram tratados com catch): */
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION!');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
