b479f93847c83ff186ebf0da38770e756e2500e0
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-09-10 12:03:41 -0300

    fix: previne criação de usuários com campos não autorizados no signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,19 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  /**
+   * Ao usar esta abordagem, estamos explicitamente escolhendo
+   * quais campos da requisição serão usados para criar o novo
+   * usuário. Isso evita que qualquer campo não autorizado
+   * (por exemplo, permissões de administrador) seja incluído na criação do usuário,
+   * prevenindo brechas de segurança.
+   */
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
